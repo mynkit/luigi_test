8737d93100e82ef13e077ea9e519bf22b9578918
mynkit <dcm5124289@gmail.com>
2020-02-17 16:07:41 +0900

    コメント追加 + autopep8

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 '''
 Reference:
     https://dev.classmethod.jp/tool/luigi_trial/
+files:
+    .
+    └── hello_world.py
 '''
 
 import luigi
@@ -9,24 +12,27 @@ from luigi.mock import MockTarget
 
 class HelloWorldTask(luigi.Task):
     task_namespace = 'examples'
- 
+
     def requires(self):
         return WorldIsFineTask()
- 
+
     def run(self):
         print("{task} says: Hello world!".format(task=self.__class__.__name__))
- 
+
+
 class WorldIsFineTask(luigi.Task):
     task_namespace = 'examples'
- 
+
     def run(self):
         with self.output().open('w') as output:
-            print("{task} says: The world is a fine place!".format(task=self.__class__.__name__))
+            print("{task} says: The world is a fine place!".format(
+                task=self.__class__.__name__))
             # output.write('{task} says: The world is a fine place!\n'.format(task=self.__class__.__name__))
- 
+
     def output(self):
         return MockTarget("output")
         # return luigi.LocalTarget('world.txt')
- 
+
+
 if __name__ == '__main__':
     luigi.run(['examples.HelloWorldTask', '--workers', '1', '--local-scheduler'])
